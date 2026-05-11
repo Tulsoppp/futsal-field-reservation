b1f3dd5ba9308343e578ff6cdd382db01045987f
suryamaulana98 <suryamaulana757@gmail.com>
2026-05-11 13:29:23 +0700

    menambahkan branch reservasi

---
+++ database/migrations/2026_05_11_061809_create_reservasis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservasis', function (Blueprint $table) {
+            $table->id();
+            $table->id('id_reservasi');
+            $table->id('id_jadwal');
+            $table->string('total_harga');
+            $table->status('status_boking');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservasis');
+    }
+};
